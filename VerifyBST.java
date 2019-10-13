bcbec3f7e02b361407329e2c18194d8dba2bc21e
Mounica <mounica2502@gmail.com>
2019-10-12 17:24:09 -0700

    add problem desc

--- VerifyBST.java
+++ VerifyBST.java
@@ -1,4 +1,13 @@
-public class verifyBST {
+/* PROBLEM - STATMENT
+Given a binary tree, determine if it is a valid binary search tree (BST).
+
+Assume a BST is defined as follows:
+The left subtree of a node contains only nodes with keys less than the node's key.
+The right subtree of a node contains only nodes with keys greater than the node's key.
+Both the left and right subtrees must also be binary search trees.
+ */
+
+public class verifyBSTSol {
     public boolean isValidBST(TreeNode node) {
         return isValidBST(node, Long.MAX_VALUE, Long.MIN_VALUE);
     }
--- WordLadder.java
+++ WordLadder.java
@@ -1,4 +1,36 @@
-public class WordLadder {
+/* PROBLEM - STATMENT
+Version-1
+Given two words (beginWord and endWord), and a dictionary's word list, find the length of shortest transformation sequence from beginWord to endWord, such that:
+
+Only one letter can be changed at a time.
+Each transformed word must exist in the word list. Note that beginWord is not a transformed word.
+Note:
+
+Return 0 if there is no such transformation sequence.
+All words have the same length.
+All words contain only lowercase alphabetic characters.
+You may assume no duplicates in the word list.
+You may assume beginWord and endWord are non-empty and are not the same.
+
+Version-2 (similar to 1 with minimal changes)
+A gene string can be represented by an 8-character long string, with choices from "A", "C", "G", "T".
+
+Suppose we need to investigate about a mutation (mutation from "start" to "end"), where ONE mutation is defined as ONE single character changed in the gene string.
+
+For example, "AACCGGTT" -> "AACCGGTA" is 1 mutation.
+
+Also, there is a given gene "bank", which records all the valid gene mutations. A gene must be in the bank to make it a valid gene string.
+
+Now, given 3 things - start, end, bank, your task is to determine what is the minimum number of mutations needed to mutate from "start" to "end". If there is no such a mutation, return -1.
+
+Note:
+
+Starting point is assumed to be valid, so it might not be included in the bank.
+If multiple mutations are needed, all mutations during in the sequence must be valid.
+You may assume start and end string is not the same.
+ */
+
+public class WordLadderSol {
     public int WordLadderLength(String beginWord, String endWord, Set<String> wordList) {
         Queue<String> queue = new LinkedList<>();
         int distance = 1;
